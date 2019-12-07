cf33d261a5b0fb0b12722c1ba7ab17f13686ef89
Ang Xu <angxuprofessional@gmail.com>
2019-12-06 18:31:10 -0600

    Auth0 initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+
+import history from "./components/utils/history";
+
+import { useAuth0 } from "./components/auth/react-auth0-spa";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
