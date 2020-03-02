d6813578509fcebafa3639ded304cd982aa1c7dc
Joel Perez <joelness03@gmail.com>
2020-03-02 10:56:18 -0700

    removed history from App component

--- src/App.js
+++ src/App.js
@@ -5,26 +5,21 @@ import { Router, Route, Switch } from "react-router-dom";
 // import Profile from "./components/Profile";
 // import PrivateRoute from "./components/PrivateRoute";
 
-
-import history from "./components/utils/history";
-
 import Login from "./components/auth/Login";
 import SignupForm from "./components/auth/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Router history={history}>
-        {/* <header>
+      {/* <header>
           <NavBar />
         </header> */}
-        <Switch>
-          <Route path="/" exact />
-          {/* <PrivateRoute path="/profile" component={Profile} /> */}
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignupForm} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact />
+        {/* <PrivateRoute path="/profile" component={Profile} /> */}
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignupForm} />
+      </Switch>
     </div>
   );
 }
