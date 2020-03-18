3abafb91c771f65612f06c24fe63161583d06f08
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-18 12:52:23 -0400

    saving changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,35 @@
 import axios from 'axios';
-export const VIEW_DEVICES = "VIEW_DEVICES";
+export const VIEW_DEVICES = "VIEW_DEVICES"; // Perhaps group all action types together, but there may be many of them,
+                                            // So breaking them up by related reducer might be the way to go.
 export const VIEW_REQUESTS = "VIEW_REQUESTS";
-//  error? 
+//  error action type? 
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+export const SUCCESS = "SUCCESS";
 
 const view_devices = () => dispatch => {
-  dispatch({type: VIEW_DEVICES});
+  dispatch({type: VIEW_DEVICES})
   axios.get('http://enterprise-devices.herokuapp.com/api/devices')
-       .then()
-       .catch((err) => {
-        console.log({error_message: err.message, error: "There was an error returning device information."})
+       .then(response => {
+         dispatch({type: SUCCESS, payload: response.data});
        })
+       .catch(err => {
+        console.log({error_message: err.message, error: "There was an error returning device information."})
+       });
+}
+
+const view_requests = () => dispatch => {
+  dispatch({type: VIEW_REQUESTS});
+  axios.get('http://enterprise-devices.herokuapp.com/api/requests')
+       .then(response => {
+        dispatch({type: SUCCESS, payload: response.data});
+       })
+       .catch(err => {
+         console.log({error_message: err.message, error: "There was an error viewing requests."})
+       })
+}
+
+const login = (value) => dispatch => {
+  dispatch({type: LOGIN});
+  axios.get() // urrrrrl
 }
