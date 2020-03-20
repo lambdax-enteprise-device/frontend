6cea64b32715a75e647609608ffbf310a88ba6cc
Miguel Peralta <eudysperalta723@gmail.com>
2020-03-20 10:21:51 -0700

    handle change and all inputs

--- src/components/EquipmentRequest.js
+++ src/components/EquipmentRequest.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const EquipmentRequest = (props) => {
+    const [ request , SetRequest ] = useState({name:"",employee:"",email:""})
+
+    const handleChange = (e) => {
+        SetRequest({...request, [e.target.name] : e.target.value})
+    }
+
+    console.log(request)
+
+
+
     return (
         <div>
             <form>
                 <label>
-                    hello
-                    <input />
+                    Equipment Name
+                    <input 
+                        type="text"
+                        name="name"
+                        onChange={handleChange}
+                        value={request.name}
+                    />
+                </label>
+                <label>
+                    employee Name
+                    <input 
+                        type="text"
+                        name="employee"
+                        onChange={handleChange}
+                        value={request.employee}
+                    />
+                </label>
+                <label>
+                    Email
+                    <input 
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        value={request.email}
+                    />
                 </label>
                 
             </form>
