a40c003c707f2222ebe78a03c1f0c601f73a30da
Doug <me@douglasbowen.com>
2020-04-01 15:23:29 -0400

    add basic material UI styling

--- src/components/inventory/Card.js
+++ src/components/inventory/Card.js
@@ -1,14 +1,28 @@
 import React from "react";
-import "./cardStyles.css";
+//import "./cardStyles.css";
+import Card from '@material-ui/core/Card';
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Card(props) {
+const useStyles = makeStyles({
+  root: {
+    marginTop: 10,
+    marginBottom: 5,
+    maxWidth: 250,
+    display: 'flex',
+    flexFlow: 'wrap',
+    justifyContent: 'center'
+  }
+})
+
+export default function Cards(props) {
+  const classes = useStyles();
   console.log(props.inventoryItems);
   return (
-    <div className='singleCard'>
-      <h4>Card: {props.inventoryItems.id} </h4>
+    <Card className={classes.root} elevation={6}>
+      
       <p>Company: {props.inventoryItems.company_id}</p>
       <p>Serial Number: {props.inventoryItems.serial_number}</p>
       <p>Internal Id: {props.inventoryItems.internal_id}</p>
-    </div>
+    </Card>
   );
 }
--- src/components/inventory/Inventory.js
+++ src/components/inventory/Inventory.js
@@ -1,41 +1,93 @@
 // Inventory component
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Card from "./Card";
+import Cards from "./Card";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: 10,
+    marginBottom: 5
+  }
+}));
 
 const Inventory = () => {
   const [inventory, setInventory] = useState([]);
-
+  const [filteredInventory, setFilteredInventory] = useState([]);
+  const classes = useStyles();
   useEffect(() => {
     axios
       .get("http://localhost:4545/api/devices")
       .then(res => setInventory(res.data))
       .catch(err => console.log(err));
-  }, []);
+  }, [filteredInventory]);
 
   const companyOne = e => {
-    setInventory(inventory.filter(item => item.company_id === 1));
+    setFilteredInventory(inventory.filter(item => item.company_id === 1));
   };
 
   const companyTwo = e => {
-    setInventory(inventory.filter(item => item.company_id === 2));
+    setFilteredInventory(inventory.filter(item => item.company_id === 2));
   };
 
   const companyThree = e => {
-    setInventory(inventory.filter(item => item.company_id === 3));
+    setFilteredInventory(inventory.filter(item => item.company_id === 3));
+  };
+
+  const allCompanies = e => {
+    setFilteredInventory(inventory);
   };
 
   return (
     <div>
-      <button onClick={companyOne}>Company 1</button>
-      <button onClick={companyTwo}>Company 2</button>
-      <button onClick={companyThree}>Company 3</button>
+      <Container maxWidth='md'>
+        <Grid
+          container
+          width={10}
+          direction='row'
+          justify='space-evenly'
+          alignItems='space-between'
+          className={classes.root}
+        >
+          <Button
+            className={classes.submit}
+            color='primary'
+            onClick={allCompanies}
+            variant='contained'
+          >
+            All Companies
+          </Button>
+          <Button color='primary' variant='contained' onClick={companyOne}>
+            Company 1
+          </Button>
+          <Button color='primary' variant='contained' onClick={companyTwo}>
+            Company 2
+          </Button>
+          <Button color='primary' variant='contained' onClick={companyThree}>
+            Company 3
+          </Button>
+        </Grid>
+        <Grid
+          container
+          width={10}
+          direction='column'
+          justify='center'
+          alignItems='center'
+        >
+          <p>List of items (all of it):</p>
 
-      <p>List of items (all of it):</p>
-
-      {inventory.map(inventoryItem => (
-        <Card key={inventoryItem.id} inventoryItems={inventoryItem} />
-      ))}
+          {filteredInventory.length == 0
+            ? inventory.map(inventoryItem => (
+                <Cards key={inventoryItem.id} inventoryItems={inventoryItem} />
+              ))
+            : filteredInventory.map(inventoryItem => (
+                <Cards key={inventoryItem.id} inventoryItems={inventoryItem} />
+              ))}
+        </Grid>
+      </Container>
     </div>
   );
 };
