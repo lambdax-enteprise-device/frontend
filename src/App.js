dbe2f47029fd237080c436e575f230f48c2aa673
mchrupcala <michael.chrupcala@gmail.com>
2020-04-10 16:34:07 -0400

    Added JSX, styling rules for the pending requests approval form for managers. added a RequestList function to loop and render all pending requests.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
+import './App.css';
 
 import NavBar from "./components/NavBar";
 import Profile from "./components/Profile";
@@ -10,6 +11,7 @@ import Login from "./components/Login/Login"
 import history from "./components/utils/history";
 
 import { useAuth0 } from "./components/auth/react-auth0-spa";
+import ManagerApproval from "./components/ManagerApproval";
 
 function App() {
   const { loading } = useAuth0();
@@ -25,9 +27,10 @@ function App() {
           <NavBar />
         </header>
         <Switch>
-          <Route path="/" exact />
+          {/* <Route path="/" exact /> */}
           <Route exact path="/login" component={Login} />
-        <Route exact path="/registration" component={Registration} />
+          <Route path="/approval" component={ManagerApproval} />
+          <Route exact path="/registration" component={Registration} />
           <PrivateRoute path="/profile" component={Profile} />
         </Switch>
       </Router>
--- src/components/ManagerApproval.js
+++ src/components/ManagerApproval.js
@@ -2,10 +2,36 @@ import React from 'react';
 
 
 const ManagerApproval = () => {
+
+    //Function takes in an array of pending requests from a different component (from props)...adds it to its own array, can manipulate each request if needed...then displays it.
+    const requestList = (pendingRequests) => {        
+        let requests = [];
+        for (let i = 0; i < pendingRequests.length; i++) {
+            requests.push(pendingRequests[i])
+        }
+
+        return requests
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="request-list">
+                {requestList(['Macbook', 'Blackberry', 'Macbook Air', 'Windows'])}
+            </div>
+
+            <div className="approval-form">
+                <h2>Employee Requests (Pending Manager's Approval) </h2>
+                <div className="manager-label-section">
+                    <label>Device Requested</label>
+                    <input type="text"></input>
+                </div>
+
+                <div className="manager-label-section">
+                    <label>Approved?</label>
+                    <input type="checkbox"></input>
+                </div>
+            </div>
+        </>
     )
 }
 
