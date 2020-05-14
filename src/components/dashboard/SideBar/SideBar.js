147422075e609453f8b0f40c0ef19949f01574d3
olympia <owojcik7@gmail.com>
2020-05-14 12:51:49 -0400

    Made drawer 100% height in Dashboard

--- src/components/dashboard/SideBar/SideBar.js
+++ src/components/dashboard/SideBar/SideBar.js
@@ -39,11 +39,11 @@ const useStyles = makeStyles(theme => ({
   },
   drawerPaper: {
     position: "fixed",
-    top: "80px",
+    top: "100",
     left: 0,
     whiteSpace: "nowrap",
     width: drawerWidth,
-    height: "80vh",
+    height: "100%",
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen
@@ -90,8 +90,8 @@ const SideBar = props => {
             {open ? (
               <ChevronLeftIcon style={{ color: "#ff9800" }} />
             ) : (
-              <ChevronRightIcon style={{ color: "#ff9800" }} />
-            )}
+                <ChevronRightIcon style={{ color: "#ff9800" }} />
+              )}
           </IconButton>
         </div>
         <Divider />
